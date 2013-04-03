389761e16a2ed60b16ed0d3081604376f9a7e6f9
Cesar Cruz <cesarcruz91@gmail.com>
2013-04-02 20:55:02 -0400

    Added remote version of firefox plugin, seems to be working fine. Morecomments are in order

---
+++ Firefox_PLugin_Remote/data/inject.js
@@ -0,0 +1,45 @@
+//Author: César F. Andreu
+//Slight modifications are made to be compatible with the firefox version of the plugin
+
+$.get('http://localhost:3000/checkLicense', function(response) {
+  console.log('Response is: ' + response.hasLicense);
+  if(!response.hasLicense){
+
+    console.log('This user does not have a valid cookie.');
+
+    //Appends the modal, hide the page, and activate the modal.
+    $('body').append("<div id='quiz' class='modal fade'> <div class='modal-header'> <h3>Quiz</h3> </div> <div class='modal-body'> <form id='quizForm'><label class='control-label'>Do you pass the quiz?</label><label class='radio inline'><input type='radio' name='question' value='yes'> Yes</label><label class='radio inline'><input type='radio' name='question' value='no'> No</label></form> </div> <div class='modal-footer'> <button id='quizSubmit' class='btn'>Submit</button> </div> </div>");
+    $('body > :not(#quiz)').hide();
+    $('#quiz').modal({backdrop: true, keyboard: false, show: true});
+
+
+    //Sets a click event listener on the submit button for the modal.
+    var button = document.getElementById('quizSubmit');
+    button.addEventListener('click', function() {
+
+      console.log('Value is:' + $("#quizForm input[type='radio']:checked").val());
+
+      if($("#quizForm input[type='radio']:checked").val()==='yes'){
+
+        $.post('http://localhost:3000/submitLicense', {submitLicense: true}, function(data){
+
+            console.log('Yes');
+            console.log('DATA: ' + data.saved);
+            location.reload();
+
+        });
+
+      }
+      else{
+        console.log('No');
+        location.reload();
+      }
+
+    }, false);
+
+  } else {
+
+    console.log('This user has a valid cookie.');
+
+  }
+});
